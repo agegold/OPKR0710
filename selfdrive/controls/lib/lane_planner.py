67e8d35dcf680d63fbfe9c0084c589570e4c3f67
chanhojung <jungch@naver.com>
2020-11-10 16:20:06 +0900

    OPKR Update

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -27,7 +27,7 @@ def calc_d_poly(l_poly, r_poly, p_poly, l_prob, r_prob, lane_width, v_ego):
 #def calc_d_poly(l_poly, r_poly, p_poly, l_prob, r_prob, lane_width, v_ego, l_std=0.05, r_std=0.05):
   # This will improve behaviour when lanes suddenly widen
   # these numbers were tested on 2000segments and found to work well
-  lane_width = min(3.8, lane_width)
+  lane_width = min(4.0, lane_width)
   width_poly = l_poly - r_poly
   prob_mods = []
   for t_check in [0.0, 1.5, 3.0]:
@@ -62,9 +62,9 @@ class LanePlanner():
     self.p_poly = [0., 0., 0., 0.]
     self.d_poly = [0., 0., 0., 0.]
 
-    self.lane_width_estimate = 3.25
+    self.lane_width_estimate = 3.7
     self.lane_width_certainty = 1.0
-    self.lane_width = 3.25
+    self.lane_width = 3.7
 
     self.l_prob = 0.
     self.r_prob = 0.
@@ -159,7 +159,7 @@ class LanePlanner():
     self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
     current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
     self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [2.5, 3.5])
+    speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
     self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
                       (1 - self.lane_width_certainty) * speed_lane_width
 
--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -600,9 +600,18 @@ static void ui_draw_debug(UIState *s)
     }
     nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_MIDDLE);
     ui_print(s, ui_viz_rx_center, ui_viz_ry+650, "커브");
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "%.4f", scene.curvature);
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+750, " 좌측간격(%%)        차선폭(m)        우측간격(%%)");
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%4.1f                    %4.2f                    %4.1f", (scene.pathPlan.lPoly/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100); 
+    if (scene.curvature >= 0.0001) {
+      ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "↖%.4f", abs(scene.curvature));
+    } else if (scene.curvature <= -0.0001) {
+      ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "%.4f↗", abs(scene.curvature));
+    } else {
+      ui_print(s, ui_viz_rx_center, ui_viz_ry+700, "%.4f", abs(scene.curvature));      
+    }
+    // ui_print(s, ui_viz_rx_center, ui_viz_ry+750, " 좌측간격(%%)        차선폭(m)        우측간격(%%)");
+    // ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%4.1f                    %4.2f                    %4.1f", (scene.pathPlan.lPoly/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100); 
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+750, " 좌측간격(m)    차선폭(m)    우측간격(m)");
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%.2f                    %.2f                    %.2f", (scene.pathPlan.lPoly - 0.94), scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)- 0.94));
+  }
   }
 }
 
