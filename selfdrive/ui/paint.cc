dd07dd09e4b8f18dc3cf8bd7fe80c8723b05c288
chanhojung <jungch@naver.com>
2020-11-11 13:38:21 +0900

    Update paint.cc

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -608,8 +608,8 @@ static void ui_draw_debug(UIState *s)
     }
     // ui_print(s, ui_viz_rx_center, ui_viz_ry+750, "←좌측간격(%%)→    차선폭(m)    ←우측간격(%%)→");
     // ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%4.1f                     %4.2f                    %4.1f", (scene.pathPlan.lPoly/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100); 
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+750, "←L-Gap→            Width(m)            ←R-Gap→");
-    ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%5.1f(cm)                       %4.2f(cm)                      %5.1f(cm)", 
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+750, "←LeftGap→            Width(m)            ←RightGap→");
+    ui_print(s, ui_viz_rx_center, ui_viz_ry+800, "%5.1f(cm)                     %4.2f(cm)                    %5.1f(cm)", 
                                   (scene.pathPlan.lPoly - 0.75)*100, scene.pathPlan.laneWidth, (abs(scene.pathPlan.rPoly)- 0.75)*100);
     ui_print(s, ui_viz_rx_center, ui_viz_ry+850, "%4.1f(%%)                                               %4.1f(%%)", 
                                   (scene.pathPlan.lPoly/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100, (abs(scene.pathPlan.rPoly)/(scene.pathPlan.lPoly+abs(scene.pathPlan.rPoly)))*100); 
