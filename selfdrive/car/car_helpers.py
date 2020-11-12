6020e86d1e8f2d7d5df70329a0774c399cf66c61
chanhojung <jungch@naver.com>
2020-11-12 13:12:43 +0900

    Update car_helpers.py

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -179,8 +179,8 @@ def get_car(logcan, sendcan, has_relay=False):
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
-    candidate = "mock"
-  # candidate = "GRANDEUR IG HEV 2020"
+    candidate = "GRANDEUR IG HEV 2020"  #candidate = "mock"
+  
 
   CarInterface, CarController, CarState = interfaces[candidate]
   car_params = CarInterface.get_params(candidate, fingerprints, has_relay, car_fw)
